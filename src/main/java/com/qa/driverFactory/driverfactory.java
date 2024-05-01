c8b44e566910dc1e2aa8de50bcef4706211c81d0
mohanty14 <45348870+mohanty14@users.noreply.github.com>
2024-05-02 04:57:06 +0530

    hello

--- src/main/java/com/qa/driverFactory/driverfactory.java
+++ src/main/java/com/qa/driverFactory/driverfactory.java
@@ -9,7 +9,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class driverfactory {
 public WebDriver driver;
   public static ThreadLocal<WebDriver> tlDriver=new ThreadLocal<WebDriver>();
-  
+  //driver
   
   public WebDriver init_driver(String browser) {
 	  System.out.println("browser value is :" + browser);
